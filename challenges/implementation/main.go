f625774c721a1cfcf058f86b78ff92ca5b7df186
erikvim <erikdavid.hedman21@outlook.com>
2024-05-02 14:59:51 -0400

    Added more examples

--- challenges/implementation/main.go
+++ challenges/implementation/main.go
@@ -4,7 +4,39 @@ import (
 	"challenges/imp/struct"
 	"fmt"
 	"log"
+	"math"
 )
+
+type Squarey struct {
+	Length float64
+}
+
+func (s Squarey) Area() float64 {
+	return s.Length * s.Length
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func sumAreas(shapes []Shape) float64 {
+	total := 0.0
+
+	for _, shape := range shapes {
+		total += shape.Area()
+	}
+
+	return total
+}
+
+// an interface defines a set of methods a type should implement:
+type Shape interface {
+	Area() float64
+}
 
 func main() {
 	s, err := structs.NewSquare(1, 1, 10)
@@ -15,4 +47,15 @@ func main() {
 	s.Move(2, 3)
 	fmt.Printf("%+v\n", s)
 	fmt.Println(s.Area())
+
+	m := Squarey{20}
+	fmt.Println(m.Area())
+
+	c := Circle{10}
+	fmt.Println(c.Area())
+
+	shapes := []Shape{m, c}
+	sa := sumAreas(shapes)
+
+	fmt.Println(sa)
 }
